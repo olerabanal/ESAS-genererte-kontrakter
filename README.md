8e1a47b5a9afeced1cd4793b74def59c77bfdfea
Andreas Strand <strand118@hotmail.com>
2023-03-06 12:18:44 +0100

    Valider json schema (#35)
    * Valider json schema
    
    * Flytt eksempelfiler for begjæring til egen mappe, og forvent tilsvarende struktur i json-schema validator
    
    * Kast Error med feilmeldinger om validering feiler
    
    * Legg til validering for kjennelse
    
    * Valider json-schema i strict-mode
    
    * Valider json-schema i github-actions-pipeline
    
    * Forklar validering av json-schema i README.md

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # ESAS
+
+Samling av kontrakter for meldinger som skal utveksles i ESAS-prosjektet.
+
+## Validering av JSON-schema
+
+Validering av JSON-schema skjer ved pull-requester og push på main-branch, med `npm run json:validate`.
+
+For å legge til validering av nye schema, utvides variabelen `jsonSchemaFolders` i `validateJsonSchema.js`.
+Scriptet forventer at det refereres til mapper som først inneholder versjoner, 
+ som igjen inneholder json-schema og en mappe med json-eksempelfiler. Eksempelvis: 
+
+```
+└── kontrakt
+    ├── 1.0
+    │   ├── foo.schema.json
+    │   └── eksempelfiler
+    │       ├── foo-eksempel-1.json
+    │       └── foo-eksempel-2.json
+    └── 1.1
+        ├── foo.schema.json
+        └── eksempelfiler
+            ├── foo-eksempel-1.json
+            └── foo-eksempel-2.json
+```
---
+++ validateJsonSchema.js
@@ -0,0 +1,68 @@
+const Ajv2020 = require("ajv/dist/2020")
+const addFormats = require('ajv-formats')
+const fs = require('fs')
+
+const ajv = new Ajv2020(opts = {strict: true})
+addFormats(ajv)
+
+const jsonSchemaFolders = [
+    "kontrakter/politi/varetekt",
+    "kontrakter/da/varetekt"
+]
+
+const isDirectory = (path) => {
+    return fs.statSync(path).isDirectory()
+}
+
+const isJsonSchema = (filename) => {
+    return filename.endsWith('.schema.json')
+}
+
+const getJsonSchema = (directory) => {
+    return directory + '/' + fs.readdirSync(directory).find(isJsonSchema)
+}
+
+const getJsonExamples = (directory) => {
+    return fs.readdirSync(directory)
+        .filter(file => file.endsWith('.json') && !file.endsWith('.schema.json'))
+        .map(file => directory + '/' + file)
+}
+
+const readFileContent = (filePath) => {
+    return {
+        file: filePath,
+        json: JSON.parse(fs.readFileSync(filePath).toString())
+    }
+}
+
+const validateJsonSchemas = (directory) => {
+    console.log("Finding JSON-schema in " + directory)
+    const jsonSchema = getJsonSchema(directory)
+    console.log("Loading JSON-schema " + jsonSchema)
+    const jsonSchemaValidator = ajv.compile(readFileContent(jsonSchema).json)
+
+    const jsonExamples = getJsonExamples(directory + "/eksempelfiler")
+        .map(readFileContent)
+
+    jsonExamples.forEach(exampleJson => {
+        console.log("Validating " + exampleJson.file + " using " + jsonSchema)
+        const validationResult = jsonSchemaValidator(exampleJson.json)
+        if (!validationResult) {
+            throw Error(jsonSchemaValidator.errors.map(error => error.instancePath + " " + error.message).join("\n"))
+        }
+    })
+
+    console.log("Successfully validated " + jsonSchema + "\n")
+}
+
+const findAndValidate = (schemaFolderBasePath) => {
+    const folderContents = fs.readdirSync(schemaFolderBasePath)
+        .map(content => schemaFolderBasePath + '/' + content)
+
+        folderContents
+            .filter(d => d !== "target")
+            .filter(isDirectory)
+            .forEach(validateJsonSchemas)
+}
+
+jsonSchemaFolders.forEach(findAndValidate)
