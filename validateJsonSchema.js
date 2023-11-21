a412b8e6bc31a842b91e83d1debc261feaeb402a
Jostein Klakegg <josklake@kriminalomsorg.no>
2023-11-21 14:55:37 +0100

    Kontrakter tilhørende forretningsprosess/tjeneste/område 'Rekvisisjon av Personundersøkelse' er nå samlet i egen frittstående mappe uten relasjon til 'aktøer'. Informasjon om produsent og konsument og andre forhold kan dokumenteres i readme eller lignende.

--- validateJsonSchema.js
+++ validateJsonSchema.js
@@ -15,8 +15,8 @@ const jsonSchemaFolders = [
   "kontrakter/felles/kvittering",
   "kontrakter/felles/kodeverk",
   "kontrakter/konfliktraadet/oppdatertsaksstatus",
-  "kontrakter/fagomraader/personundersoekelse/rekvisisjonPersonundersoekelse",
-  "kontrakter/fagomraader/personundersoekelse/returPersonundersoekelse",
+  "kontrakter/personundersoekelse/rekvisisjonPersonundersoekelse",
+  "kontrakter/personundersoekelse/returPersonundersoekelse",
 ];
 
 const jsonKodeverkFolders = ["kodeverk/felles"];
